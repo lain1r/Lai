b9232f387c199e45ea045e1713f663692f76159f
Maxim <aidmax1234@gmail.com>
2024-11-14 10:37:49 +0300

    sdadssss

---
+++ src/lesson1/massiv.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class massiv {
+    public static void main(String[] args) {
+        int[]arr1 = new int[10];
+        arr1[1] = 5;
+        arr1[2] = 55;
+        arr1[3] = 555;
+        arr1[4] = 5555;
+        arr1[5] = 55555;
+    }
+}
